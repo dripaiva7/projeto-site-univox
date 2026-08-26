2a13f9292431da0ee4019e3660f8ab2d536c60be
unknown <dri.paiva7@gmail.com>
2026-08-26 15:08:44 -0300

    Estrutura inicial Vs Code site Univox

---
+++ src/main/java/com/quarteto/Univox_site/UnivoxSiteApplication.java
@@ -0,0 +1,13 @@
+package com.quarteto.Univox_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivoxSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivoxSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quarteto/Univox_site/UnivoxSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quarteto.Univox_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivoxSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
