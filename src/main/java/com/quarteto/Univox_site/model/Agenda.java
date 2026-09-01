6f219376ea09498f53701e8697879cfb003ead11
unknown <dri.paiva7@gmail.com>
2026-09-01 17:18:07 -0300

    Criei a entidade Agenda

---
+++ src/main/java/com/quarteto/Univox_site/model/Agenda.java
@@ -0,0 +1,101 @@
+package com.quarteto.Univox_site.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Agenda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String titulo;
+    String descricao;
+    String local;
+    LocalDate data;
+    String cidade;
+    LocalTime horario;
+    String estado;
+    String link;
+
+
+    public Agenda(Integer id, String titulo, String descricao, String local, LocalDate data, String cidade,
+            LocalTime horario, String estado, String link) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.local = local;
+        this.data = data;
+        this.cidade = cidade;
+        this.horario = horario;
+        this.estado = estado;
+        this.link = link;
+    }
+
+     public Agenda() {
+    }
+
+    
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public LocalTime getHorario() {
+        return horario;
+    }
+    public void setHorario(LocalTime horario) {
+        this.horario = horario;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+  
+    
+}
